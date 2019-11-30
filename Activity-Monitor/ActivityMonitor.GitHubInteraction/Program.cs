da7f5ee904da09ae4e3f7e46196e2009a8ad4f29
Sergey <ogresed@gmail.com>
2019-11-30 12:38:44 +0700

    github interaction initial commit

---
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Octokit;
+
+namespace ActivityMonitor.GitHubInteraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientId = "6efbf777d1c4c3f1862e";
+            var clientSecret = "a460bed91ffc7e1b3b9ace4d39ddfc9aaef70654";
+            var client = new GitHubClient(new ProductHeaderValue("Activity Monitor"));
+            Console.ReadKey();
+        }
+    }
+}

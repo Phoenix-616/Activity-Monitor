8a25d5e44dfaeabbc478ec49d6b174c3ba668ae0
Sergey <ogresed@gmail.com>
2020-01-07 23:39:47 +0700

    added data getting

--- Activity-Monitor/ActivityMonitor.GitHubInteraction/Crawler.cs
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/Crawler.cs
@@ -1,11 +1,64 @@
 ﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace ActivityMonitor.GitHubInteraction
 {
-    class Crawler
+    public class Crawler
     {
         public readonly string NameOfApp = "Activity-Monitor";
-        private readonly GitHubClient client;       
+        private readonly GitHubClient client;
 
+        public class Data
+        {
+            public string CommitSha { get; set; }
+            public string CommitAuthorName { get; set; }
+            public string CommitAuthorLogin { get; set; }
+            public string CommitAuthorEmail { get; set; }
+            public DateTimeOffset CreatedAt { get; set; }
+            public string RepositoryName { get; set; }
+            public IEnumerable<string> FilesNames { get; set; }
+            public int Additions { get; set; }
+            public int Deletions { get; set; }
+        }
+        private async Task<Data> GetData(GitHubCommit commit, string owner, string name)
+        {
+            var data = new Data
+            {
+                CommitSha = commit.Sha,
+                CommitAuthorName = commit.Commit.Author.Name,
+                CommitAuthorEmail = commit.Commit.Author.Email,
+                CreatedAt = commit.Commit.Author.Date,
+                RepositoryName = owner + "/" + name,
+            };
+            var author = commit.Author;
+            var login = author == null ? null : author.Login;
+            data.CommitAuthorLogin = login;
+
+            var currentCommit = await client.Repository.Commit.Get(owner, name, data.CommitSha);
+            var filesNames = currentCommit.Files.Select(x => x.Filename);
+            data.FilesNames = filesNames;
+
+            int additions = 0;
+            int deletions = 0;
+
+            var additionsArrayByCommit = currentCommit.Files.Select(x => x.Additions);
+            var deletionsArrayByCommit = currentCommit.Files.Select(x => x.Deletions);
+            foreach (var a in additionsArrayByCommit)
+            {
+                additions += a;
+            }
+            foreach (var d in deletionsArrayByCommit)
+            {
+                deletions += d;
+            }
+
+            data.Additions = additions;
+            data.Deletions = deletions;
+            return data;
+        }
         public Crawler(string login, string password)
         {
             client = new GitHubClient(new ProductHeaderValue(NameOfApp));
--- Activity-Monitor/ActivityMonitor/GitDatabaseSeeder.cs
+++ Activity-Monitor/ActivityMonitor/GitDatabaseSeeder.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ActivityMonitor.Database;
+using ActivityMonitor.GitHubInteraction;
 
 namespace ActivityMonitor
 {
     class GitDatabaseSeeder : DatabaseSeeder
     {
+        private Crawler crawler;
+        RepositoryAttribute[] attributes;
         public GitDatabaseSeeder(ActivityContext context,
                                  string login,
                                  string password,
                                  RepositoryAttribute[] attributes) : base(context)
         {
+            crawler = new Crawler(login, password);
+            this.attributes = attributes;
         }
 
         public override Task Seed()
